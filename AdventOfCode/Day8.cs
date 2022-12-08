c1ddf1d4ddadc0ad7dfb98dcaf7f8e853bf7e9e4
David Weber <david.weber99@seznam.cz>
2022-12-08 07:38:08 +0100

    Completed Day 8

---
+++ AdventOfCode/Day8.cs
@@ -0,0 +1,155 @@
+﻿
+namespace AdventOfCode
+{
+    internal class Day8
+    {
+        enum Direction
+        {
+            Up, Down, Left, Right
+        }
+        public static int GetScenicScore(string treeMap)
+        {
+
+            int[][] trees = treeMap
+                .Split(Environment.NewLine)
+                .Select(x => x.ToCharArray().Select(x => x.ToString()).Select(x => int.Parse(x)).ToArray())
+                .ToArray();
+
+            bool WithinBounds(int i, int j)
+            {
+                return i >= 0 && i < trees.Length &&
+                    j >= 0 && j < trees[0].Length;
+            }
+
+            int GetNonBlockingTrees(int i, int j, Direction direction)
+            {
+                int tree = 0;
+                int n = 0;
+                int m = 0;
+                int res = 0;
+                switch (direction)
+                {
+                    case Direction.Left:
+                        n = i;
+                        m = j - 1;
+                        break;
+                    case Direction.Right:
+                        n = i;
+                        m = j + 1;
+                        break;
+                    case Direction.Up:
+                        n = i - 1;
+                        m = j;
+                        break;
+                    case Direction.Down:
+                        n = i + 1;
+                        m = j;
+                        break;
+                }
+                while (true)
+                {
+                    if (!WithinBounds(n, m))
+                        break;
+
+                    tree = trees[n][m];
+                    res++;
+                    if (tree >= trees[i][j])
+                        break;
+
+                    switch (direction)
+                    {
+                        case Direction.Left: m--; break;
+                        case Direction.Right: m++; break;
+                        case Direction.Up: n--; break;
+                        case Direction.Down: n++; break;
+                    }
+                }
+                return res;
+            }
+
+            int scenicScore = 0;
+            int rowCount = trees.Length;
+            int columnCount = trees[0].Length;
+            for (int i = 0; i < rowCount; i++)
+                for (int j = 0; j < columnCount; j++)
+                    scenicScore = Math.Max(scenicScore,
+                        GetNonBlockingTrees(i, j, Direction.Left) *
+                        GetNonBlockingTrees(i, j, Direction.Right) *
+                        GetNonBlockingTrees(i, j, Direction.Down) *
+                        GetNonBlockingTrees(i, j, Direction.Up));
+
+
+            return scenicScore;
+        }
+        public static int GetVisibleTrees(string treeMap)
+        {
+
+            int[][] trees = treeMap
+                .Split(Environment.NewLine)
+                .Select(x => x.ToCharArray().Select(x => x.ToString()).Select(x => int.Parse(x)).ToArray())
+                .ToArray();
+
+            bool WithinBounds(int i, int j)
+            {
+                return i >= 0 && i < trees.Length &&
+                    j >= 0 && j < trees[0].Length;
+            }
+
+            bool IsVisible(int i, int j, Direction direction)
+            {
+                int tree = 0;
+                int n = 0;
+                int m = 0;
+                switch (direction)
+                {
+                    case Direction.Left:
+                        n = i;
+                        m = j - 1;
+                        break;
+                    case Direction.Right:
+                        n = i;
+                        m = j + 1;
+                        break;
+                    case Direction.Up:
+                        n = i - 1;
+                        m = j;
+                        break;
+                    case Direction.Down:
+                        n = i + 1;
+                        m = j;
+                        break;
+                }
+                while (true)
+                {
+                    if (!WithinBounds(n, m))
+                        break;
+
+                    tree = trees[n][m];
+                    if (tree >= trees[i][j])
+                        return false;
+                    
+                    switch (direction)
+                    {
+                        case Direction.Left: m--; break;
+                        case Direction.Right: m++; break;
+                        case Direction.Up: n--; break;
+                        case Direction.Down: n++; break;
+                    }
+                }
+                return true;
+            }
+
+            int visibleTrees = 0;
+            int rowCount = trees.Length;
+            int columnCount = trees[0].Length;
+            for (int i = 0; i < rowCount; i++)
+                for (int j = 0; j < columnCount; j++)
+                    visibleTrees += IsVisible(i, j, Direction.Left) ||
+                        IsVisible(i, j, Direction.Right) ||
+                        IsVisible(i, j, Direction.Up) ||
+                        IsVisible(i, j, Direction.Down) ? 1 : 0;
+                    
+            return visibleTrees;
+        }
+    }
+}
--- AdventOfCode/Program.cs
+++ AdventOfCode/Program.cs
@@ -4,47 +4,55 @@
     {
         static void Main(string[] args)
         {
+            string inputFolder = "PuzzleInputs\\";
+
             // #1
             Console.WriteLine("#1");
-            string callories = File.ReadAllText("1.txt");
+            string callories = File.ReadAllText(inputFolder + "1.txt");
             Console.WriteLine("Max callories: " + Day1.MaxCallories(callories));
             Console.WriteLine("Top Three Max Callories: " + Day1.TopThreeMaxCallories(callories) + "\n");
 
             // #2
             Console.WriteLine("#2");
-            string strategy = File.ReadAllText("2.txt");
+            string strategy = File.ReadAllText(inputFolder + "2.txt");
             Console.WriteLine("Total Points from strategy: " + Day2.RockPaperScissorsTotal(strategy));
             Console.WriteLine("Total Points from strategy (part 2): " + Day2.RockPaperScissorsTotal2(strategy) + "\n");
 
             // #3
             Console.WriteLine("#3");
-            string rucksacks = File.ReadAllText("3.txt");
+            string rucksacks = File.ReadAllText(inputFolder + "3.txt");
             Console.WriteLine("Rucksacks Total Item Priority: " + Day3.RucksackSum(rucksacks));
             Console.WriteLine("Rucksacks Total Item Priority (part 2): " + Day3.RucksackSum2(rucksacks) + "\n");
 
             // #4
             Console.WriteLine("#4");
-            string pairs = File.ReadAllText("4.txt");
+            string pairs = File.ReadAllText(inputFolder + "4.txt");
             Console.WriteLine("Overlaping pairs: " + Day4.Overlaps(pairs));
-            Console.WriteLine("Overlaping pairs (part 2): " + Day4.Overlaps2(pairs));
+            Console.WriteLine("Overlaping pairs (part 2): " + Day4.Overlaps2(pairs) + "\n");
 
             // #5
             Console.WriteLine("#5");
-            string initConfig = File.ReadAllText("5.txt");
+            string initConfig = File.ReadAllText(inputFolder + "5.txt");
             Console.WriteLine("Stack Tops: " + Day5.CrateRearangement(initConfig));
-            Console.WriteLine("Stack Tops (part 2): " + Day5.CrateRearangement2(initConfig));
+            Console.WriteLine("Stack Tops (part 2): " + Day5.CrateRearangement2(initConfig) + "\n");
 
             // #6
             Console.WriteLine("#6");
-            string data = File.ReadAllText("6.txt");
+            string data = File.ReadAllText(inputFolder + "6.txt");
             Console.WriteLine("Marker Position: " + Day6.GetMarkerPosition(data, 4));
-            Console.WriteLine("Marker Position (part 2): " + Day6.GetMarkerPosition(data, 14));
+            Console.WriteLine("Marker Position (part 2): " + Day6.GetMarkerPosition(data, 14) + "\n");
 
             // #7
             Console.WriteLine("#7");
-            string console = File.ReadAllText("7.txt");
+            string console = File.ReadAllText(inputFolder + "7.txt");
             Console.WriteLine("Dirs with max size (100000): " + Day7.DirectoriesToDelete(console, 100000));
-            Console.WriteLine("Smallest dir to delete: " + Day7.SmallestDirectoryToDelete(console, 70000000, 30000000));
+            Console.WriteLine("Smallest dir to delete: " + Day7.SmallestDirectoryToDelete(console, 70000000, 30000000) + "\n");
+
+            // #8
+            Console.WriteLine("#8");
+            string treeMap = File.ReadAllText(inputFolder + "8.txt");
+            Console.WriteLine("Visible trees: " + Day8.GetVisibleTrees(treeMap));
+            Console.WriteLine("Scenic score: " + Day8.GetScenicScore(treeMap));
         }
     }
 }
