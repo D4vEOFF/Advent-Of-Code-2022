62d3a4561c8b3a1f8fd2f638dc343b3e759c650e
David Weber <david.weber99@seznam.cz>
2022-12-15 20:37:14 +0100

    Completed Day 15

---
+++ AdventOfCode/Day15.cs
@@ -0,0 +1,111 @@
+﻿
+namespace AdventOfCode
+{
+    internal class Day15
+    {
+        class Sensor
+        {
+            public Point2D Position { get; private set; }
+            public Point2D ClosestBeacon { get; private set; }
+            public int Manhattan
+            {
+                get => Math.Abs(Position.X - ClosestBeacon.X) 
+                    + Math.Abs(Position.Y - ClosestBeacon.Y);
+            }
+            public Sensor(Point2D position, Point2D closestBeacon)
+            {
+                Position = position;
+                ClosestBeacon = closestBeacon;
+            }
+        }
+        private static Sensor[] ParseInput(string input)
+        {
+            input = input.Replace("Sensor at ", "").Replace(": closest beacon is at ", ":");
+            return input
+                .Split(Environment.NewLine)
+                .Select(x =>
+                {
+                    string[] line = x.Split(":");
+                    string[] sensorCoords = line[0].Split(", ");
+                    Point2D sensorPos = new Point2D(int.Parse(sensorCoords[0].Remove(0, 2)),
+                        int.Parse(sensorCoords[1].Remove(0, 2)));
+                    string[] beaconCoords = line[1].Split(", ");
+                    Point2D beaconPos = new Point2D(int.Parse(beaconCoords[0].Remove(0, 2)),
+                        int.Parse(beaconCoords[1].Remove(0, 2)));
+
+                    return new Sensor(sensorPos, beaconPos);
+                })
+                .ToArray();
+        }
+        internal static int GetPossiblyEmptyPositions(string map, int line)
+        {
+            Sensor[] sensors = ParseInput(map);
+
+            HashSet<Point2D> points = new HashSet<Point2D>();
+            foreach (var sensor in sensors)
+            {
+                int residualManhattan = sensor.Manhattan - Math.Abs(sensor.Position.Y - line);
+
+                // Check line
+                for (int x = sensor.Position.X - residualManhattan; 
+                    x <= sensor.Position.X + residualManhattan; x++)
+                {
+                    Point2D pos = new Point2D(x, line);
+
+                    // Beacon is present on the checked position
+                    if (pos == sensor.ClosestBeacon)
+                        continue;
+
+                    points.Add(pos);
+                }
+            }
+
+            return points.Count;
+        }
+        private static int GetManhattan(Point2D p1, Point2D p2)
+        {
+            return Math.Abs(p1.X - p2.X) + Math.Abs(p1.Y - p2.Y);
+        }
+        internal static ulong GetTuningFrequency(string map)
+        {
+            Sensor[] sensors = ParseInput(map);
+
+            // Positive/negative borders (lines)
+            List<int> positiveLines = new List<int>();
+            List<int> negativeLines = new List<int>();
+            foreach (var sensor in sensors)
+            {
+                positiveLines.Add(sensor.Position.X - sensor.Position.Y + sensor.Manhattan);
+                positiveLines.Add(sensor.Position.X - sensor.Position.Y - sensor.Manhattan);
+
+                negativeLines.Add(sensor.Position.X + sensor.Position.Y + sensor.Manhattan);
+                negativeLines.Add(sensor.Position.X + sensor.Position.Y - sensor.Manhattan);
+            }
+
+            int positive = 0;
+            int negative = 0;
+
+            // Loop through all pairs of pos/neg lines
+            for (int i = 0; i < 2 * sensors.Length; i++)
+                for (int j = i + 1; j < 2 * sensors.Length; j++)
+                {
+                    int line1 = positiveLines[i];
+                    int line2 = positiveLines[j];
+
+                    if (Math.Abs(line1 - line2) == 2)
+                        positive = Math.Min(line1, line2) + 1;
+
+                    line1 = negativeLines[i];
+                    line2 = negativeLines[j];
+
+                    if (Math.Abs(line1 - line2) == 2)
+                        negative = Math.Min(line1, line2) + 1;
+                }
+
+            int x = (negative + positive) / 2;
+            int y = (negative - positive) / 2;
+
+            return 4000000 * (ulong)x + (ulong)y;
+        }
+    }
+}
--- AdventOfCode/Program.cs
+++ AdventOfCode/Program.cs
@@ -4,91 +4,100 @@
     {
         static void Main(string[] args)
         {
+            int makePart = 1;
+            try
+            {
+                Console.Write("Day number: ");
+                makePart = int.Parse(Console.ReadLine());
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
             string inputFolder = "PuzzleInputs\\";
 
-            // #1
-            Console.WriteLine("#1");
-            string callories = File.ReadAllText(inputFolder + "1.txt");
-            Console.WriteLine("Max callories: " + Day1.MaxCallories(callories));
-            Console.WriteLine("Top Three Max Callories: " + Day1.TopThreeMaxCallories(callories) + "\n");
-
-            // #2
-            Console.WriteLine("#2");
-            string strategy = File.ReadAllText(inputFolder + "2.txt");
-            Console.WriteLine("Total Points from strategy: " + Day2.RockPaperScissorsTotal(strategy));
-            Console.WriteLine("Total Points from strategy (part 2): " + Day2.RockPaperScissorsTotal2(strategy) + "\n");
-
-            // #3
-            Console.WriteLine("#3");
-            string rucksacks = File.ReadAllText(inputFolder + "3.txt");
-            Console.WriteLine("Rucksacks Total Item Priority: " + Day3.RucksackSum(rucksacks));
-            Console.WriteLine("Rucksacks Total Item Priority (part 2): " + Day3.RucksackSum2(rucksacks) + "\n");
-
-            // #4
-            Console.WriteLine("#4");
-            string pairs = File.ReadAllText(inputFolder + "4.txt");
-            Console.WriteLine("Overlaping pairs: " + Day4.Overlaps(pairs));
-            Console.WriteLine("Overlaping pairs (part 2): " + Day4.Overlaps2(pairs) + "\n");
-
-            // #5
-            Console.WriteLine("#5");
-            string initConfig = File.ReadAllText(inputFolder + "5.txt");
-            Console.WriteLine("Stack Tops: " + Day5.CrateRearangement(initConfig));
-            Console.WriteLine("Stack Tops (part 2): " + Day5.CrateRearangement2(initConfig) + "\n");
-
-            // #6
-            Console.WriteLine("#6");
-            string data = File.ReadAllText(inputFolder + "6.txt");
-            Console.WriteLine("Marker Position: " + Day6.GetMarkerPosition(data, 4));
-            Console.WriteLine("Marker Position (part 2): " + Day6.GetMarkerPosition(data, 14) + "\n");
-
-            // #7
-            Console.WriteLine("#7");
-            string console = File.ReadAllText(inputFolder + "7.txt");
-            Console.WriteLine("Dirs with max size (100000): " + Day7.DirectoriesToDelete(console, 100000));
-            Console.WriteLine("Smallest dir to delete: " + Day7.SmallestDirectoryToDelete(console, 70000000, 30000000) + "\n");
-
-            // #8
-            Console.WriteLine("#8");
-            string treeMap = File.ReadAllText(inputFolder + "8.txt");
-            Console.WriteLine("Visible trees: " + Day8.GetVisibleTrees(treeMap));
-            Console.WriteLine("Scenic score: " + Day8.GetScenicScore(treeMap));
-
-            // #9
-            Console.WriteLine("#9");
-            string headMovement = File.ReadAllText(inputFolder + "9.txt");
-            Console.WriteLine("Head visited positions: " + Day9.GetTailVisitedPositions(headMovement, 2));
-            Console.WriteLine("Head visited positions (part 2): " + Day9.GetTailVisitedPositions(headMovement, 10) + "\n");
-
-            // #10
-            Console.WriteLine("#10");
-            string code = File.ReadAllText(inputFolder + "10.txt");
-            Console.WriteLine("Signal strength: " + Day10.GetSignalStrength(code, 20, 40, 220));
-            Console.WriteLine(Day10.GetScreen(code) + "\n");
-
-            // #11
-            Console.WriteLine("#11");
-            string monkeysAttributes = File.ReadAllText(inputFolder + "11.txt");
-            Console.WriteLine("Monkey Business: " + Day11.GetMonkeyBusiness(monkeysAttributes, 20, true));
-            Console.WriteLine("Monkey Business (part 2): " + Day11.GetMonkeyBusiness(monkeysAttributes, 10000, false));
-
-            // #12
-            Console.WriteLine("#12");
-            string map = File.ReadAllText(inputFolder + "12.txt");
-            Console.WriteLine("Shortest path to top: " + Day12.GetShortestPathToTop(map));
-            Console.WriteLine("Shortest path to top (part 2): " + Day12.GetShortestPathToTopFromGround(map));
-
-            // #13
-            Console.WriteLine("#13");
-            string packets = File.ReadAllText(inputFolder + "13.txt");
-            Console.WriteLine("Wrong packets order indices: " + Day13.GetWrongOrderIndices(packets));
-            Console.WriteLine("Decoder Key: " + Day13.GetDecoderKey(packets));
-
-            // #14
-            Console.WriteLine("#14");
-            string cave = File.ReadAllText(inputFolder + "14.txt");
-            Console.WriteLine("Sand units at rest: " + Day14.GetUnitsOfSandAtRest(cave, false));
-            Console.WriteLine("Sand units at rest (part 2): " + Day14.GetUnitsOfSandAtRest(cave, true));
+            switch (makePart)
+            {
+                case 1:
+                    string callories = File.ReadAllText(inputFolder + "1.txt");
+                    Console.WriteLine("Max callories: " + Day1.MaxCallories(callories));
+                    Console.WriteLine("Top Three Max Callories: " + Day1.TopThreeMaxCallories(callories) + "\n");
+                    break;
+                case 2:
+                    string strategy = File.ReadAllText(inputFolder + "2.txt");
+                    Console.WriteLine("Total Points from strategy: " + Day2.RockPaperScissorsTotal(strategy));
+                    Console.WriteLine("Total Points from strategy (part 2): " + Day2.RockPaperScissorsTotal2(strategy) + "\n");
+                    break;
+                case 3:
+                    string rucksacks = File.ReadAllText(inputFolder + "3.txt");
+                    Console.WriteLine("Rucksacks Total Item Priority: " + Day3.RucksackSum(rucksacks));
+                    Console.WriteLine("Rucksacks Total Item Priority (part 2): " + Day3.RucksackSum2(rucksacks) + "\n");
+                    break;
+                case 4:
+                    string pairs = File.ReadAllText(inputFolder + "4.txt");
+                    Console.WriteLine("Overlaping pairs: " + Day4.Overlaps(pairs));
+                    Console.WriteLine("Overlaping pairs (part 2): " + Day4.Overlaps2(pairs) + "\n");
+                    break;
+                case 5:
+                    string initConfig = File.ReadAllText(inputFolder + "5.txt");
+                    Console.WriteLine("Stack Tops: " + Day5.CrateRearangement(initConfig));
+                    Console.WriteLine("Stack Tops (part 2): " + Day5.CrateRearangement2(initConfig) + "\n");
+                    break;
+                case 6:
+                    string data = File.ReadAllText(inputFolder + "6.txt");
+                    Console.WriteLine("Marker Position: " + Day6.GetMarkerPosition(data, 4));
+                    Console.WriteLine("Marker Position (part 2): " + Day6.GetMarkerPosition(data, 14) + "\n");
+                    break;
+                case 7:
+                    string console = File.ReadAllText(inputFolder + "7.txt");
+                    Console.WriteLine("Dirs with max size (100000): " + Day7.DirectoriesToDelete(console, 100000));
+                    Console.WriteLine("Smallest dir to delete: " + Day7.SmallestDirectoryToDelete(console, 70000000, 30000000) + "\n");
+                    break;
+                case 8:
+                    string treeMap = File.ReadAllText(inputFolder + "8.txt");
+                    Console.WriteLine("Visible trees: " + Day8.GetVisibleTrees(treeMap));
+                    Console.WriteLine("Scenic score: " + Day8.GetScenicScore(treeMap));
+                    break;
+                case 9:
+                    string headMovement = File.ReadAllText(inputFolder + "9.txt");
+                    Console.WriteLine("Head visited positions: " + Day9.GetTailVisitedPositions(headMovement, 2));
+                    Console.WriteLine("Head visited positions (part 2): " + Day9.GetTailVisitedPositions(headMovement, 10) + "\n");
+                    break;
+                case 10:
+                    string code = File.ReadAllText(inputFolder + "10.txt");
+                    Console.WriteLine("Signal strength: " + Day10.GetSignalStrength(code, 20, 40, 220));
+                    Console.WriteLine(Day10.GetScreen(code) + "\n");
+                    break;
+                case 11:
+                    string monkeysAttributes = File.ReadAllText(inputFolder + "11.txt");
+                    Console.WriteLine("Monkey Business: " + Day11.GetMonkeyBusiness(monkeysAttributes, 20, true));
+                    Console.WriteLine("Monkey Business (part 2): " + Day11.GetMonkeyBusiness(monkeysAttributes, 10000, false));
+                    break;
+                case 12:
+                    string map = File.ReadAllText(inputFolder + "12.txt");
+                    Console.WriteLine("Shortest path to top: " + Day12.GetShortestPathToTop(map));
+                    Console.WriteLine("Shortest path to top (part 2): " + Day12.GetShortestPathToTopFromGround(map));
+                    break;
+                case 13:
+                    string packets = File.ReadAllText(inputFolder + "13.txt");
+                    Console.WriteLine("Wrong packets order indices: " + Day13.GetWrongOrderIndices(packets));
+                    Console.WriteLine("Decoder Key: " + Day13.GetDecoderKey(packets));
+                    break;
+                case 14:
+                    string cave = File.ReadAllText(inputFolder + "14.txt");
+                    Console.WriteLine("Sand units at rest: " + Day14.GetUnitsOfSandAtRest(cave, false));
+                    Console.WriteLine("Sand units at rest (part 2): " + Day14.GetUnitsOfSandAtRest(cave, true));
+                    break;
+                case 15:
+                    string beacons = File.ReadAllText(inputFolder + "15.txt");
+                    Console.WriteLine("Empty positions: " + Day15.GetPossiblyEmptyPositions(beacons, 2000000));
+                    Console.WriteLine("Tuning frequency: " + Day15.GetTuningFrequency(beacons));
+                    break;
+                default:
+                    Console.WriteLine("Invalid day number.");
+                    break;
+            }
         }
     }
 }
